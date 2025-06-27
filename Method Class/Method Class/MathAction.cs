57082152e371adb1edcae50d92ef9b468ffbb201
AidanCollins55 <aidancollins55@gmail.com>
2025-06-27 12:03:09 -0700

    Method Class

---
+++ Method Class/Method Class/MathAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// Define a class named MathAction
+public class MathAction
+{
+    // This is a void method that takes two integers as input parameters
+    public void PerformOperation(int firstNumber, int secondNumber)
+    {
+        // Perform a math operation on the first number (e.g., square it)
+        int result = firstNumber * firstNumber;
+
+        // Display the result of the math operation
+        Console.WriteLine($"The square of the first number ({firstNumber}) is: {result}");
+
+        // Display the second number
+        Console.WriteLine($"The second number is: {secondNumber}");
+    }
+}
---
+++ Method Class/Method Class/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an instance of the MathAction class to use its method
+        MathAction action = new MathAction();
+
+    
+        // Pass in two numbers directly (positional parameters)
+        action.PerformOperation(4, 10); // 4 will be squared, 10 will be printed
+
+        
+        // This way we specify the name of each parameter
+        action.PerformOperation(firstNumber: 7, secondNumber: 3); // 7 will be squared, 3 will be printed
+    }
+}
+
