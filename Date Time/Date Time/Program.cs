f4acab46ae6febafc4ac8bd1c22fdd43747cb911
AidanCollins55 <aidancollins55@gmail.com>
2025-07-01 16:50:45 -0700

    Date/Time
    This program gives you the time and asks the user to input how many hours we would like to add. It then adds those hours and gives you what the time would be.

---
+++ Date Time/Date Time/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Get the current date and time from the system
+        DateTime currentDateTime = DateTime.Now;
+
+        // Print the current date and time to the console
+        Console.WriteLine("Current Date and Time: " + currentDateTime);
+
+        // Ask the user to input a number of hours to add
+        Console.Write("\nEnter a number of hours to add: ");
+        string userInput = Console.ReadLine(); // Read user input as a string
+
+        // Convert the user input to an integer (assuming valid numeric input)
+        int hoursToAdd = int.Parse(userInput);
+
+        // Calculate the future time by adding the specified number of hours
+        DateTime futureDateTime = currentDateTime.AddHours(hoursToAdd);
+
+        // Print the calculated future date and time to the console
+        Console.WriteLine("\nThe time in " + hoursToAdd + " hour(s) will be: " + futureDateTime);
+    }
+}
